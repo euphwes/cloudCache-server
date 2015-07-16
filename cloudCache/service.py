87d1628a5942d9944712f164ff946da11c550385
Wes Evans <euphwes@gmail.com>
2015-07-16 16:08:13 -0600

    Added Sublime Text project file, and cloudCache source package. Created empty class CacheItemHandler in service.py which will implement the REST API actions.

---
+++ cloudCache/service.py
@@ -0,0 +1,9 @@
+""" A REST API for the cloudCache application. """
+
+import tornado.web
+
+# -------------------------------------------------------------------------------------------------
+
+class CacheItemHandler(tornado.web.RequestHandler):
+    """ The main API handler for creating, updating, and retrieving cloudCache entries. """
+    pass
