d45c4769b5f5c736d504fdd4f82053468a7bbe16
Wes Evans <euphwes@gmail.com>
2015-07-17 19:08:14 -0400

    Added extremely basic echo GET method to CacheItemHandler for demo/testing purposes.

--- cloudCache/service.py
+++ cloudCache/service.py
@@ -1,9 +1,25 @@
 """ A REST API for the cloudCache application. """
 
 import tornado.web
+import tornado.ioloop
 
 # -------------------------------------------------------------------------------------------------
 
 class CacheItemHandler(tornado.web.RequestHandler):
     """ The main API handler for creating, updating, and retrieving cloudCache entries. """
-    pass
+
+    def get(self, key, value):
+        print("{}: {}".format(key, value))
+
+# -------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    routes = [(r"/cache/(.+)/(.+)", CacheItemHandler)]
+
+    application = tornado.web.Application(routes)
+    application.listen(8888)
+
+    tornado.ioloop.IOLoop.instance().start()
+
+# curl -I -X GET http://localhost:8888/cache/name/wes
