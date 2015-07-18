4ccb86ffd3ccbd810bb3880590aef2603cbeed85
Wes Evans <euphwes@gmail.com>
2015-07-18 16:17:12 -0600

    Moved code in the ifmain block into its own method. Pulled the port that the server listens on out to a constant value.

--- cloudCache/service.py
+++ cloudCache/service.py
@@ -5,6 +5,10 @@ import tornado.ioloop
 
 # -------------------------------------------------------------------------------------------------
 
+SERVER_PORT = 8888
+
+# -------------------------------------------------------------------------------------------------
+
 class CacheItemHandler(tornado.web.RequestHandler):
     """ The main API handler for creating, updating, and retrieving cloudCache entries. """
 
@@ -13,13 +17,19 @@ class CacheItemHandler(tornado.web.RequestHandler):
 
 # -------------------------------------------------------------------------------------------------
 
-if __name__ == '__main__':
+def main():
+    """ Runs the server. """
 
     routes = [(r"/cache/(.+)/(.+)", CacheItemHandler)]
 
     application = tornado.web.Application(routes)
-    application.listen(8888)
+    application.listen(SERVER_PORT)
 
     tornado.ioloop.IOLoop.instance().start()
+
+# -------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    main()
 
 # curl -I -X GET http://localhost:8888/cache/name/wes
