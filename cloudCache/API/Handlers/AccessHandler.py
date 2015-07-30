ac8f01dfacc4653063399e2dcb8460a775411854
Wes Evans <euphwes@gmail.com>
2015-07-30 15:34:11 -0600

    Cleaned up AccessHandler.get(), now using appropriate HTTP status codes for errors, and cleaned up response format.

--- cloudCache/API/Handlers/AccessHandler.py
+++ cloudCache/API/Handlers/AccessHandler.py
@@ -2,7 +2,6 @@
 
 from . import AuthorizeHandler
 
-from cloudCache.Business.Models import User, UserAccessToken, DB_SESSION as db
 from cloudCache.Business.Models.UserAccessToken import create_access_token
 from cloudCache.Business.Errors import InvalidApiKeyError, UserDoesntExistError
 
@@ -12,15 +11,21 @@ class AccessHandler(AuthorizeHandler):
     """ The request handler for creating and delivering cloudCache UserAccessTokens. """
 
     def get(self, username, api_key):
+        """ Handles creating an access token for a given username, if they submit the correct API
+        key. If the key is invalid, or the user doesn't exist, and appropriate error message is
+        returned with the correct HTTP status code. If everything works out, the access token is
+        returned. """
 
         try:
             token = create_access_token(username, api_key)
-            response = {
-                'status'      : 'OK',
-                'access token': token.to_ordered_dict()
-            }
+            response = {'access token': token.to_ordered_dict()}
 
-        except (InvalidApiKeyError, UserDoesntExistError) as e:
-            response = self.get_failure_response(e)
+        except UserDoesntExistError as e:
+            self.set_status(404) # Not Found
+            response = {'message': str(e)}
+
+        except InvalidApiKeyError as e:
+            self.set_status(401) # unauthenticated
+            response = {'message': str(e)}
 
         self.write(response)
